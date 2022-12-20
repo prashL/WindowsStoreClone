dec12734ad984f3476d9ac82fe44cdb5933de0f3
prashL <prashanth.qwerty@ymail.com>
2022-12-20 14:38:44 +0530

    EP81,Completed Home Page

--- WindowsStoreClone/Pages/Main.xaml.cs
+++ WindowsStoreClone/Pages/Main.xaml.cs
@@ -12,25 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WindowsStoreClone.Pages;
 
-namespace WindowsStoreClone
+namespace WindowsStoreClone.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Main.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Main : Page
     {
-        Main MainWindowContentPage;
-        public MainWindow()
+        public Main()
         {
             InitializeComponent();
-            MainWindowContentPage = new Main();
-        }
-
-        private void MainWindowFrame_Loaded(object sender, RoutedEventArgs e)
-        {
-            MainWindowFrame.Content = MainWindowContentPage;
         }
     }
 }
